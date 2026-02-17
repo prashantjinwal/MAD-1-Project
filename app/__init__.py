be1e3690df4c2b1d28df3e5b0441757fb3d3a7b1
prashantjinwal <prashantjinwal888@gmail.com>
2026-02-17 21:15:55 +0530

    Add register logic

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,18 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from werkzeug.security import generate_password_hash
+from app.extensions import db, login_manager
 
-db = SQLAlchemy()
-login_manager = LoginManager()
 
- 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
 
     db.init_app(app)
     login_manager.init_app(app)
+
+    from app.auth import auth
+    app.register_blueprint(auth)
 
     with app.app_context():
         from app import models
@@ -31,3 +31,11 @@ def create_admin():
         )
         db.session.add(admin)
         db.session.commit()
+
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models import User
+    return User.query.get(int(user_id))
+
+
+ 
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,36 +1,38 @@
 from flask import  render_template, url_for, redirect, request, flash, session
 from werkzeug.security import generate_password_hash
-from app import db
+from app.extensions import db
+from app.models import User, Student
+from app.auth import auth
 
 
-@auth.route("/register/student", method=['GET', 'POST'])
+@auth.route("/register/student", methods=['GET', 'POST'])
 def register_student():
      if request.method == 'POST':
-        email = request.form.get("email")
-        password = request.form.get("password")
-        name = request.form.get("name")
-        roll_no = request.form.get('roll_no')
+         email = request.form.get("email")
+         password = request.form.get("password")
+         name = request.form.get("name")
+         roll_no = request.form.get('roll_no')
 
-        if User.query.filter_by(email=email).first():
-          flash("Already register")
-          return redirect(url_for("auth.register_student"))
-        user  = User(
-          email = email,
-          password= generate_password_hash(password),
-          role="student"
+         if User.query.filter_by(email=email).first():
+           flash("Already register")
+           return redirect(url_for("auth.register_student"))
+         user  = User(
+           email = email,
+           password= generate_password_hash(password),
+           role="student"
+         )
+         db.session.add(user)
+         db.session.commit()
+
+         student = Student(
+            user_id=user.id,
+            name=name,
+            roll_no=roll_no
         )
-        db.session.add(user)
-        db.session.commit()
+         db.session.add(student)
+         db.session.commit()
 
-        student = Student(
-          user_id=user.id,
-          name=name,
-          roll_no=roll_no
-        )
-        db.session.add(student)
-        db.session.commit()
+         flash("register successfully, Please login.")
+         return redirect(url_for("auth.login"))
 
-        flash("register successfully, Please login.")
-        return redirect(url_for("auth.login"))
-
-     return render_template("register_student.html")
+     return render_template("auth/register.html")
